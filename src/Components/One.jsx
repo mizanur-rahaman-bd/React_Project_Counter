77a149527149e54e4bef171d4e1d8a1f99ae92de
mizanur-rahaman-bd <sumon1581@gmail.com>
2025-03-07 22:13:47 +0600

    simple

--- src/Components/One.jsx
+++ src/Components/One.jsx
@@ -1,19 +1,26 @@
-import React from 'react'
-import './One.css'
+import React from "react";
+import "./One.css";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment, reset } from "../Slice/counterSlice";
 
 const One = () => {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
     <>
-    <div className="container">
+      <div className="container">
         <div className="main_counter">
-            <h1>hi</h1>
-
+          <button onClick={() => dispatch(increment())}>+</button>
+          <h2>{count}</h2>
+          <button onClick={() => dispatch(decrement())}>-</button>
+          <div className="reset_button">
+            <button onClick={() => dispatch(reset())}>Reset</button>
+          </div>
         </div>
-    </div>
-
-    
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default One
+export default One;
