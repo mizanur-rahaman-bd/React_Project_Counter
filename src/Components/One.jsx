4ba7965cadddef1389a7d7d71c36af71ccfde301
mizanur-rahaman-bd <sumon1581@gmail.com>
2025-03-11 22:31:51 +0600

    without_res

--- src/Components/One.jsx
+++ src/Components/One.jsx
@@ -11,23 +11,23 @@ const One = () => {
     <>
       <div className="container">
         <div className="heading mx-auto">
-          <h1 className="mx-auto">Counter Mizanur Rahaman</h1>
-        </div>
-        <div className="counterpage">
           
         </div>
-        <div className="main_counter">
-          <button className="decrease" onClick={() => dispatch(decrement())}>
-            -
-          </button>
-          <h2 className="number">{count}</h2>
-          <button className="increase" onClick={() => dispatch(increment())}>
-            +
-          </button>
+        <div className="counterpage w-[310px] h-[248px] md:w-[500px] md:h-[400px]">
+        <h1 className="curve_text text-center">COUNTER</h1>
+          <div className="main_counter">
+            <button className="decrease" onClick={() => dispatch(decrement())}>
+              -
+            </button>
+            <h2 className="number">{count}</h2>
+            <button className="increase" onClick={() => dispatch(increment())}>
+              +
+            </button>
+          </div>
+          <div className="reset_button">
+            <button onClick={() => dispatch(reset())}>RESET</button>
+          </div>
         </div>
-      </div>
-      <div className="reset_button">
-        <button onClick={() => dispatch(reset())}>RESET</button>
       </div>
     </>
   );
